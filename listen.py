88e87f0313543bc5fd3bb649ed509849fcd14abf
GawliYash20 <137262372+GawliYash20@users.noreply.github.com>
2023-09-01 10:26:22 +0530

    Add files via upload

---
+++ listen.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+
+def Listen():
+
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as mic:
+        print("Listening. . .")
+        recognizer.pause_threshold = 1
+        audio = recognizer.listen(mic, 0, 2)
+
+
+    try:
+        print("recognizing...")
+        query = recognizer.recognize_google(audio, language="en-in")
+        print(f"You said: {query} \n")
+
+
+    except:
+        return ""
+
+    query = str(query)
+    return query.lower()
+
+Listen()
---
+++ speech.py
@@ -0,0 +1,22 @@
+import pyttsx3
+
+
+
+
+def Say(Text):
+    
+    engine = pyttsx3.init("sapi5")
+    voices = engine.getProperty('voices')
+    engine.setProperty('voices', voices[0].id)
+    engine.setProperty('rate', 170)
+    
+    # print("    ")
+    print(f"systemBuddy: {Text}")
+    engine.say(text=Text)
+    engine.runAndWait()
+    print("    ")
+
+
+
+
+
